015020185f440247b9d77c7474781c7eb01c42a1
Henry <henry_ball@hotmail.co.uk>
2024-08-15 23:15:03 +0100

    chore: fix types

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,7 +15,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       return (
         <ContentEditable
           placeholder={props.placeholder}
-          html={value || ""}
+          html={value ?? ""}
           onChange={onChange}
           className="block min-h-[70px] w-full cursor-text rounded-md border-0 bg-dark-300 bg-white/5 px-3 py-1.5 text-light-900 text-neutral-900 shadow-sm ring-1 ring-inset ring-light-600 focus:ring-2 focus:ring-inset focus:ring-light-600 focus-visible:outline-none dark:text-dark-1000 dark:ring-dark-700 dark:focus:ring-dark-700 sm:text-sm sm:leading-6"
         />
