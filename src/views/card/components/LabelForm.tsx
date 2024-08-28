677981bfefa86eb662b4b7064d7e800e79ec1e0d
Henry <henry_ball@hotmail.co.uk>
2024-08-28 10:58:07 +0100

    fix: remove whitespace

--- src/views/card/components/LabelForm.tsx
+++ src/views/card/components/LabelForm.tsx
@@ -27,7 +27,7 @@ const colours = [
   { name: "Blue", code: "#0284c7" },
   { name: "Purple", code: "#4f46e5" },
   { name: "Yellow", code: "#ca8a04" },
-  { name: "Orange", code: "#ea580c " },
+  { name: "Orange", code: "#ea580c" },
   { name: "Red", code: "#dc2626" },
   { name: "Pink", code: "#db2777" },
 ];
