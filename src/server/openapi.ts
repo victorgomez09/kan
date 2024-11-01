82b682b4001330be679c5f4ad3ac9ec78b8917ea
Henry <henry_ball@hotmail.co.uk>
2024-11-01 08:51:01 +0000

    fix: set trpc server version

--- src/server/openapi.ts
+++ src/server/openapi.ts
@@ -6,7 +6,7 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Kan API",
   description: "OpenAPI compliant REST API",
   version: "1.0.0",
-  baseUrl: "http://localhost:3000/api/v1",
+  baseUrl: `${process.env.WEBSITE_URL}/api/v1`,
   docsUrl: "",
   tags: ["auth", "users", "posts"],
 });
