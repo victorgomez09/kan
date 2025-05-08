68a5c270a66f07378cd9ac71e21a5c685956fd64
Henry <henry_ball@hotmail.co.uk>
2025-05-08 15:36:11 +0100

    fix: init auth for auth api route

--- apps/web/src/pages/api/auth/[...all].ts
+++ apps/web/src/pages/api/auth/[...all].ts
@@ -1,8 +1,12 @@
 import { toNodeHandler } from "better-auth/node";
 
-import { auth } from "@kan/auth";
+import { initAuth } from "@kan/auth";
+import { createDrizzleClient } from "@kan/db/client";
 
 export const config = { api: { bodyParser: false } };
+
+const auth = initAuth(createDrizzleClient());
+
 export default toNodeHandler(auth.handler);
 
 // export const runtime = "edge";
